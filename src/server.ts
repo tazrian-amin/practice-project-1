7bb0d79149283c3fe314fbff5afe56ce3735ba0d
tazrian-amin <amin.tazrian1979@gmail.com>
2023-11-23 10:59:32 +0600

    project set up

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+
+const { port, database_url } = config;
+
+async function main() {
+  try {
+    await mongoose.connect(database_url as string);
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
